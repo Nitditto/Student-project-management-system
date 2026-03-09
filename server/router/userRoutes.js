6ef8a534efd8405f07fd6b2253c3fc029f467e6f
FireFish <truongboboy2005@gmail.com>
2026-03-09 17:39:04 +0700

    [BACKEND] feat: Implement admin functionalities for managing students and teachers, including create, update, delete, and fetch operations

--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -1,16 +1,22 @@
-import express from 'express'
-import { forgotPassword, getUser, loginUser, logout, registerUser, resetPassword } from "../controllers/authController.js"
-import { isAuthenticated } from '../middleware/authMiddleware.js';
-import multer from 'multer'
+import express from "express";
+import {
+  forgotPassword,
+  getUser,
+  loginUser,
+  logout,
+  registerUser,
+  resetPassword,
+} from "../controllers/authController.js";
+import { isAuthenticated } from "../middleware/authMiddleware.js";
+import multer from "multer";
 
 const router = express.Router();
 
-router.post("/register", registerUser)
-router.post("/login", loginUser)
-router.get("/me", isAuthenticated, getUser)
-router.get("/logout", isAuthenticated, logout)
-router.post("/password/forgot", forgotPassword)
-router.put("/password/reset/:token", resetPassword)
-
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/me", isAuthenticated, getUser);
+router.get("/logout", isAuthenticated, logout);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
 
 export default router;
