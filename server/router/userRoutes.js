358db21d3061ca1796f14c33a6a29456dc86296c
dzungluu25 <minhanhnguyen1423@gmail.com>
2026-03-23 14:07:10 +0700

    [BACKEND] fix: refactored and added route

--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -1,16 +1,15 @@
-import express from 'express'
-import { forgotPassword, getUser, loginUser, logout, registerUser, resetPassword } from "../controllers/authController.js"
+import express from "express";
+import {registerUser, login, logout, forgotPassword, getUser, resetPassword} from "../controllers/authController.js";
+import multer from "multer";
 import { isAuthenticated } from '../middleware/authMiddleware.js';
-import multer from 'multer'
-
 const router = express.Router();
-
-router.post("/register", registerUser)
-router.post("/login", loginUser)
-router.get("/me", isAuthenticated, getUser)
-router.get("/logout", isAuthenticated, logout)
-router.post("/password/forgot", forgotPassword)
-router.put("/password/reset/:token", resetPassword)
-
+router.post("/register", registerUser);
+router.post("/login", login);
+router.get("/me", getUser);
+router.get("/me", isAuthenticated, getUser);
+router.post("/logout", logout);
+router.post("/password/forgot", forgotPassword);
+router.post("/password/reset/:token", resetPassword);
 
 export default router;
+
