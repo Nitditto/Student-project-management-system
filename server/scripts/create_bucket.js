9c37422359ff4271516f93917e1dc7a11035bfac
FireFish <truongboboy2005@gmail.com>
2026-06-25 01:41:08 +0700

    feat: add script to initialize and verify Supabase storage bucket
    abc

--- server/scripts/create_bucket.js
+++ server/scripts/create_bucket.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-globalThis.WebSocket = class {};
+globalThis.WebSocket = class { };
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SECRET_KEY;
+const supabaseKey = process.env.SUPABASE_KEY;
 const bucketName = process.env.SUPABASE_BUCKET;
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
